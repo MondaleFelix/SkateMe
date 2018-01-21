4172aba69e2bec0656bb7d5f0024a1325c38ccae
Mondale Felix <mondalefelix@yahoo.com>
2018-01-21 14:56:29 -0800

    implemented scroll view

--- SkateMe/ViewController.swift
+++ SkateMe/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate{
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var contentWidth: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        
+        for image in 0...2{
+            let imageToDisplay = UIImage(named: "\(image).png")
+            let imageView = UIImageView(image: imageToDisplay)
+            
+            let xCoordinate = view.frame.midX + view.frame.width * CGFloat(image)
+            
+            contentWidth += view.frame.width
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: xCoordinate - 250 , y: (view.frame.height / 2) - 250, width: 500, height: 500)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
     }
 
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(414))
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
